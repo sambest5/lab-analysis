b288ac07ea44074316b0fe6ec1caa021517042e7
Sam Best <samuel.best5@gmail.com>
2017-11-25 19:35:04 -0500

    updated to math portion

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function changeClass(element) {
+    if (element.className == "open"){
+        element.className = "closed";
+    }
+    else if (element.className == "closed"){
+        element.className = "open";
+    }
+}
